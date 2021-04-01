8d52d5a4ea6c85983e415ce6a9b0da45a5b8b1d5
Satie MH <32197331+SaTieMH@users.noreply.github.com>
2021-04-01 09:32:31 -0300

    Update promiseInsadeLoop.js

--- promiseInsadeLoop.js
+++ promiseInsadeLoop.js
@@ -19,3 +19,6 @@ for (let i = 0, p = Promise.resolve(); i < 10; i++) {
      });
    });
 }  
+
+//https://stackoverflow.com/questions/57321325/how-to-change-arrow-function-to-regular-function
+//https://stackoverflow.com/questions/40328932/javascript-es6-promise-for-loop/40338726#40338726?newreg=8324995125284942b43fe5ee48691927
